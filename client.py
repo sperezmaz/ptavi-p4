fce2b4c73d4fb050e467ea9dd2e4a16447fa0aad
Sergio Perez Mazadiego <sperez@gamma25.aulas.gsyc.urjc.es>
2017-10-23 11:03:42 +0200

    inicial

--- client.py
+++ client.py
@@ -3,20 +3,24 @@
 """
 Programa cliente UDP que abre un socket a un servidor
 """
-
+import sys
 import socket
 
 # Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = 'localhost'
-PORT = 6001
-LINE = '¡Hola mundo!'
+try:
+    SERVER = sys.argv[1]
+    PORT = int(sys.argv[2])
+    LINE = " ".join(sys.argv[3:])
+except ValueError:
+    sys.exit("Error: Puerto solo puede ser entero")
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket: 
     my_socket.connect((SERVER, PORT))
     print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n') #lo pasamos a bytes
+    #tambien b'\r\n' pasa a bytes
     data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
+    print('Recibido -- ', data.decode('utf-8')) #pasa a string los bytes
 
 print("Socket terminado.")
--- server.py
+++ server.py
@@ -5,6 +5,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -17,17 +18,22 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         handle method of the server class
         (all requests will be handled by this method)
         """
+        #fichero imaginario de escritura en bytes
         self.wfile.write(b"Hemos recibido tu peticion")
         for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
-
+            Puerto_Client = self.client_address[1];
+            print("La IP del cliente es: " + str(self.client_address[0]) + 
+            ", el puerto del cliente es: " + str(Puerto_Client) + 
+            " y nos manda:", line.decode('utf-8'))
+        
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001 
     # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', 6001), EchoHandler) 
-
+    #va a estar escuchando en este puerto
+    #PORT_ = int(sys.argv[1])
+    serv = socketserver.UDPServer(('', int(sys.argv[1])), EchoHandler) 
     print("Lanzando servidor UDP de eco...")
     try:
-        serv.serve_forever()
-    except KeyboardInterrupt:
+        serv.serve_forever() #espera en un bucle
+    except KeyboardInterrupt: # ^C
         print("Finalizado servidor")
