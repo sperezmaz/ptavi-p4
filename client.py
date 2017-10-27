c14f247cb46bd8c161641ef34a191fac38b70ce4
sperezmaz <s.perezmaz@alumnos.urjc.es>
2017-10-27 20:13:34 +0200

    ejercicio6

--- client.py
+++ client.py
@@ -10,16 +10,19 @@ import socket
 try:
     SERVER = sys.argv[1]
     PORT = int(sys.argv[2])
-    LINE = " ".join(sys.argv[3:])
+    TYPE_MESSAGES = sys.argv[3]
+    ADDRESS_REGIST = sys.argv[4]
+
 except ValueError:
     sys.exit("Error: Puerto solo puede ser entero")
 
+line = TYPE_MESSAGES.upper() + " sip:" + ADDRESS_REGIST + ' SIP/2.0\r\n'
+
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket: 
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n') #lo pasamos a bytes
-    #tambien b'\r\n' pasa a bytes
+    print("Enviando:", line)
+    my_socket.send(bytes(line, 'utf-8')) #lo pasamos a bytes
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8')) #pasa a string los bytes
 
--- server.py
+++ server.py
@@ -8,30 +8,35 @@ import socketserver
 import sys
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
+    dicc = {}
     def handle(self):
         """
         handle method of the server class
         (all requests will be handled by this method)
         """
         #fichero imaginario de escritura en bytes
-        self.wfile.write(b"Hemos recibido tu peticion")
+        self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+        IP_Client = self.client_address[0];
+        Port_Client = self.client_address[1];
+        print("La IP del cliente es: " + str(IP_Client) +
+              ", el puerto del cliente es: " + str(Port_Client))
         for line in self.rfile:
-            Puerto_Client = self.client_address[1];
-            print("La IP del cliente es: " + str(self.client_address[0]) + 
-            ", el puerto del cliente es: " + str(Puerto_Client) + 
-            " y nos manda:", line.decode('utf-8'))
+            message = line.decode('utf-8').split()
+            type_messages = message[0]
+            address_reg = message[1]
+        if type_messages == "REGISTER":
+            self.dicc[address_reg] = str(IP_Client)
+            print(self.dicc)
         
 if __name__ == "__main__":
-    # Listens at localhost ('') port 6001 
+    # Listens at localhost ('') port 6001
     # and calls the EchoHandler class to manage the request
-    #va a estar escuchando en este puerto
-    #PORT_ = int(sys.argv[1])
-    serv = socketserver.UDPServer(('', int(sys.argv[1])), EchoHandler) 
+    PORT_SERV = int(sys.argv[1])
+    serv = socketserver.UDPServer(('', PORT_SERV), SIPRegisterHandler)
     print("Lanzando servidor UDP de eco...")
     try:
         serv.serve_forever() #espera en un bucle
